d709b683ff2937d62c68dac1ef2069b4e10c0353
Kuralbek798 <agubaev_kuralbek@mail.ru>
2026-04-05 17:44:24 +0500

    message improve

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,15 @@ import Step from "./Step";
 import "../styles/index.css";
 import { useState } from "react";
 
-
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 
-
 function sendMessage(messages, count) {
-  return messages[count - 1];
+  return `Step ${count}: ${messages[count - 1]}`;
 }
-
 
 function App() {
   const [count, setCount] = useState(1);
@@ -21,18 +18,15 @@ function App() {
 
   function handleBtnClick(event) {
     let name = event.currentTarget.name;
-   
+
     if(name === 'close'){
       setOpen(!isOpen);
     }
-    console.log(name);
+   
     if (name === 'next' && count < 3) {
-      setCount((c) => c + 1);
-      console.log(count);
-    }
-    if (name === 'prev' && count > 1) {
-      setCount((c) => c - 1);
-      console.log(count);
+      setCount((c) => c + 1);      
+    }else if (name === 'prev' && count > 1) {
+      setCount((c) => c - 1);      
     }
   }
 
--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -3,18 +3,20 @@ import "../styles/index.css";
 
 function Step({ count, isOpen, handleBtnClick, message }) {  
   return (
-    <div>
+    <>
       <button name ="close" className="close" onClick={handleBtnClick}>
         &times;
       </button>
       {isOpen && (
-        <>
+        <div className="steps">
           <div className="numbers">
             <div className={` '' ${count >= 1 ? "active" : ""}`}>1</div>
             <div className={` '' ${count >= 2 ? "active" : ""}`}>2</div>
             <div className={` '' ${count === 3 ? "active" : ""}`}>3</div>
           </div>
+
           <p className="message">{message}</p>
+         
           <div className="buttons ">
             <button name="prev" className="btnStyle" onClick={handleBtnClick}>
               Previous
@@ -23,9 +25,10 @@ function Step({ count, isOpen, handleBtnClick, message }) {
               Next
             </button>
           </div>
-        </>
+        </div>
       )}
-    </div>
+    </>
+    
   );
 }
 
